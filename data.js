bd28da747e71a0596b07d51379453a6edc03f501
Haenuki Sachi <sachihaenuki1@gmail.com>
2024-11-14 21:50:00 +0700

    Update Program

--- data.js
+++ data.js
@@ -1,343 +1,15 @@
 export let kanji = [
   {
-    hint: "Cleaning",
-    answer: "掃除"
+    hint: "Weekend",
+    answer: "週末"
   },
   {
-    hint: "In The End/Finally/After All",
-    answer: "結局"
-  },
-  {
-    hint: "Space",
-    answer: "宇宙"
-  },
-  {
-    hint: "Piggy Bank",
-    answer: "貯金箱"
-  },
-  {
-    hint: "Transparent",
-    answer: "透明"
-  },
-  {
-    hint: "Bowl",
-    answer: "茶碗"
-  },
-  {
-    hint: "Secretary",
-    answer: "秘書"
-  },
-  {
-    hint: "Disturbance",
-    answer: "邪魔"
-  },
-  {
-    hint: "Fire Truck",
-    answer: "消防車"
-  },
-  {
-    hint: "Prevent",
-    answer: "防ぐ"
-  },
-  {
-    hint: "Laundry",
-    answer: "洗濯"
-  },
-  {
-    hint: "Enjoyable",
-    answer: "楽しい"
-  },
-  {
-    hint: "Entrance",
-    answer: "入口"
-  },
-  {
-    hint: "Population (Number)",
-    answer: "人口"
-  },
-  {
-    hint: "Private (School etc)",
-    answer: "私立"
-  },
-  {
-    hint: "Public (School etc)",
-    answer: "市立"
-  },
-  {
-    hint: "Single/Unmarried",
-    answer: "独身"
-  },
-  {
-    hint: "Over",
-    answer: "以上"
-  },
-  {
-    hint: "Under",
-    answer: "以下"
-  },
-  {
-    hint: "Greetings",
-    answer: "挨拶"
-  },
-  {
-    hint: "Television Program",
-    answer: "テレビ番組"
-  },
-  {
-    hint: "Kind",
-    answer: "親切"
-  },
-  {
-    hint: "Complicated",
-    answer: "複雑"
-  },
-  {
-    hint: "Cool/Stylish",
-    answer: "格好いい"
-  },
-  {
-    hint: "Unpleasant",
-    answer: "嫌"
-  },
-  {
-    hint: "Important (For thing)",
-    answer: "大事"
-  },
-  {
-    hint: "Important (For Human/Emotion)",
-    answer: "大切"
-  },
-  {
-    hint: "Commuting to work",
-    answer: "通勤"
-  },
-  {
-    hint: "Box",
-    answer: "箱"
-  },
-  {
-    hint: "Researcher",
-    answer: "研究者"
-  },
-  {
-    hint: "Bank Officer",
-    answer: "銀行員"
-  },
-  {
-    hint: "Excuse Me (Polite)",
-    answer: "失礼"
-  },
-  {
-    hint: "Souvenir",
-    answer: "お土産"
-  },
-  {
-    hint: "Sweets",
-    answer: "お菓子"
-  },
-  {
-    hint: "Straight",
-    answer: "真っ直ぐ"
-  },
-  {
-    hint: "Help (Care)",
-    answer: "お世話"
-  },
-  {
-    hint: "Small Notes/Small Dictionary",
-    answer: "手帳"
-  },
-  {
-    hint: "Selling Area",
-    answer: "売り場"
-  },
-  {
-    hint: "1st Underground Floor",
-    answer: "地下一階"
-  },
-  {
-    hint: "Meeting Room",
-    answer: "会議室"
-  },
-  {
-    hint: "Receptionist Desk/Information Desk",
-    answer: "受付"
-  },
-  {
-    hint: "Administrative Working Place",
-    answer: "事務所"
-  },
-  {
-    hint: "Lesson 1",
-    answer: "第一課"
-  },
-  {
-    hint: "Business Card",
-    answer: "名刺"
-  },
-  {
-    hint: "Pencil",
-    answer: "鉛筆"
-  },
-  {
-    hint: "Vending Machine",
-    answer: "自動販売機"
-  },
-  {
-    hint: "Post Office",
-    answer: "郵便局"
-  },
-  {
-    hint: "Art Museum",
-    answer: "美術館"
-  },
-  {
-    hint: "Class",
-    answer: "授業"
-  },
-  {
-    hint: "Bad Taste",
-    answer: "不味い"
-  },
-  {
-    hint: "Arrive",
-    answer: "着く"
-  },
-  {
-    hint: "GreenGrocer or Vegetable Shop",
-    answer: "八百屋"
-  },
-  {
-    hint: "etc.",
-    answer: "など"
-  },
-  {
-    hint: "Gradually",
-    answer: "だんだん"
-  },
-  {
-    hint: "Traffic Light",
-    answer: "信号"
-  },
-  {
-    hint: "Variety",
-    answer: "いろいろな"
-  },
-  {
-    hint: "Submit/Takeout",
-    answer: "出す"
-  },
-  {
-    hint: "Evening (4 to 7 PM)",
-    answer: "夕方"
-  },
-  {
-    hint: "To Become Tired",
-    answer: "疲れる"
-  },
-  {
-    hint: "Train Platform",
-    answer: "番線"
-  },
-  {
-    hint: "Normal Train",
-    answer: "普通"
-  },
-  {
-    hint: "Make Phone Call",
-    answer: "掛かる"
-  },
-  {
-    hint: "Famous/Popular",
-    answer: "有名"
-  },
-  {
-    hint: "Daily Life",
-    answer: "生活"
-  },
-  {
-    hint: "One Cup",
-    answer: "一杯"
-  },
-  {
-    hint: "Unclever",
+    hint: "Not Skill",
     answer: "下手"
   },
   {
-    hint: "Promise",
-    answer: "約束"
-  },
-  {
-    hint: "Unfortunately",
-    answer: "産穢ですね"
-  },
-  {
-    hint: "Drawing",
-    answer: "絵"
-  },
-  {
-    hint: "Not At All",
-    answer: "全然"
-  },
-  {
-    hint: "Work (Personal Task)",
-    answer: "用事"
-  },
-  {
-    hint: "Meal",
-    answer: "料理"
-  },
-  {
-    hint: "Understand",
-    answer: "分かる"
-  },
-  {
-    hint: "Stop Place",
-    answer: "乗り場"
-  },
-  {
-    hint: "Battery",
-    answer: "電池"
-  },
-  {
-    hint: "Convenient",
-    answer: "便利"
-  },
-  {
-    hint: "Garden",
-    answer: "庭"
-  },
-  {
-    hint: "Ticket",
-    answer: "切符"
-  },
-  {
-    hint: "Private (non-public)",
-    answer: "非公開"
-  },
-  {
-    hint: "Free",
-    answer: "暇"
-  },
-  {
-    hint: "Busy (City), Lively",
-    answer: "賑やか"
-  },
-  {
-    hint: "Busy (City), Lively",
-    answer: "賑やか"
-  },
-  {
-    hint: "Someone's wife",
-    answer: "奥さん"
-  },
-  {
-    hint: "My Wife (not kana)",
-    answer: "妻"
-  },
-  {
-    hint: "My Husband (not shujin)",
-    answer: "夫"
+    hint: "Unfortunate",
+    answer: "残念"
   },
   {
     hint: "Sea mail",
@@ -348,59 +20,139 @@ export let kanji = [
     answer: "航空便"
   },
   {
-    hint: "Cool (weather comfortable)",
+    hint: "Battery",
+    answer: "電池"
+  },
+  {
+    hint: "Cool",
     answer: "涼しい"
   },
   {
     hint: "Festival",
-    answer: "祭り"
+    answer: "お祭り"
+  },
+  {
+    hint: "Cloudy",
+    answer: "曇り"
   },
   {
     hint: "Spicy",
     answer: "辛い"
   },
   {
-    hint: "cloudy",
-    answer: "曇り"
-  },
-  {
     hint: "Order",
     answer: "ご注文"
   },
   {
-    hint: "Separate Bill",
-    answer: "別々"
+    hint: "Set Menu",
+    answer: "定食"
   },
   {
-    hint: "To pick up",
+    hint: "Jogging",
+    answer: "散歩"
+  },
+  {
+    hint: "Help",
+    answer: "手伝う"
+  },
+  {
+    hint: "Call/Summon",
+    answer: "呼ぶ"
+  },
+  {
+    hint: "Fall (snow/rain)",
+    answer: "降る"
+  },
+  {
+    hint: "To Pick Up",
     answer: "迎える"
-  },
-  {
-    hint: "Set menu",
-    answer: "定食"
-  },
-  {
-    hint: "Set menu",
-    answer: "定食"
-  },
-  {
-    hint: "Fishing",
-    answer: "釣り"
   },
   {
     hint: "Address",
     answer: "住所"
   },
   {
-    hint: "Timetable",
-    answer: "時刻表"
+    hint: "City Hall",
+    answer: "市役所"
   },
   {
-    hint: "Documents",
+    hint: "Materials/Resources",
     answer: "資料"
   },
   {
-    hint: "Product",
+    hint: "Product/item",
     answer: "製品"
+  },
+  {
+    hint: "Pull Out/withdrawal",
+    answer: "引き出す"
+  },
+  {
+    hint: "Password/pin",
+    answer: "暗証番号"
+  },
+  {
+    hint: "Confirmation",
+    answer: "確認"
+  },
+  {
+    hint: "The Amount of Money",
+    answer: "金額"
+  },
+  {
+    hint: "Withdraw money from ATM",
+    answer: "下ろす"
+  },
+  {
+    hint: "Fever",
+    answer: "熱"
+  },
+  {
+    hint: "Certificate",
+    answer: "保険証"
+  },
+  {
+    hint: "Getting of a vehicle",
+    answer: "降りる"
+  },
+  {
+    hint: "Height",
+    answer: "背"
+  },
+  {
+    hint: "Weight",
+    answer: "体重"
+  },
+  {
+    hint: "Weight (for Human)",
+    answer: "重さ"
+  },
+  {
+    hint: "Driving",
+    answer: "運転"
+  },
+  {
+    hint: "Book/reservation",
+    answer: "予約"
+  },
+  {
+    hint: "Cash",
+    answer: "現金"
+  },
+  {
+    hint: "Head Department",
+    answer: "部長"
+  },
+  {
+    hint: "Young",
+    answer: "若い"
+  },
+  {
+    hint: "Repair",
+    answer: "修理"
+  },
+  {
+    hint: "Useful",
+    answer: "やくにたつ"
   }
 ]
